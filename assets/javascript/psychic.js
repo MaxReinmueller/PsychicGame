b812f6a9eb06a992273510fdf8490e2f2a8ffda1
Max Reinmueller <maxreinmueller@gmail.com>
2019-07-09 14:44:08 -0700

    finished product

--- assets/javascript/psychic.js
+++ assets/javascript/psychic.js
@@ -2,7 +2,7 @@
 var wins = 0;
 var losses = 0;
 var gussesLeft = 9;
-var YourguessesSoFar = [];
+var yourguessesSoFar = [];
 var compGuess = [];
 var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var userGuess = [];
@@ -12,63 +12,55 @@ window.onload = function () {
     var computerPick = computerLetters[Math.floor(Math.random() * computerLetters.length)];
     compGuess.push(computerPick);
     console.log(compGuess[0]);
-    document.getElementById("winstxt").innerHTML =
-        "wins: " + wins;
-    document.getElementById("lossestxt").innerHTML =
-        "losses: " + losses;
-    document.getElementById("gussesLefttxt").innerHTML =
-        "Guesses Left: " + gussesLeft;
-    document.getElementById("YourguessesSoFartxt").innerHTML =
-        "Your guesses so far: " + YourguessesSoFar;
-   
+
+
 }
 
 // User Pick starts game
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    YourguessesSoFar.push(userGuess);
-    console.log(YourguessesSoFar);
-    document.getElementById("YourguessesSoFartxt").innerHTML =
-        "Your guesses so far: " + YourguessesSoFar;
-
+    yourguessesSoFar.push(userGuess);
+    console.log(yourguessesSoFar);
 
 
     // conditions for wins, losses, and how numbers are logged
     if ((userGuess === compGuess[0]) && (gussesLeft > 0)) {
         wins++;
         gussesLeft = 9;
-        compGuess.length = 0;
-        YourguessesSoFar.length = 0;
+        yourguessesSoFar = [];
+        compGuess = [];
         var computerPick = computerLetters[Math.floor(Math.random() * computerLetters.length)];
         compGuess.push(computerPick);
         console.log(compGuess[0]);
-        document.getElementById("winstxt").innerHTML =
-            "wins: " + wins;
-        document.getElementById("YourguessesSoFartxt").innerHTML =
-            "Your guesses so far: " + YourguessesSoFar;
-    } 
-    else if ((YourguessesSoFar = compGuess[0]) && (gussesLeft > 0)) {
+    } else if ((yourguessesSoFar !== compGuess[0]) && (gussesLeft > 0)) {
         gussesLeft = gussesLeft - 1;
-        document.getElementById("gussesLefttxt").innerHTML =
-            "Guesses Left: " + gussesLeft;
-    } 
-    else {
+
+
+    } else {
         losses++;
         gussesLeft = 9;
-        compGuess.length = 0;
-        YourguessesSoFar.length = 0;
+        yourguessesSoFar = [];
+        compGuess = [];
         var computerPick = computerLetters[Math.floor(Math.random() * computerLetters.length)];
         compGuess.push(computerPick);
         console.log(compGuess[0]);
-        document.getElementById("YourguessesSoFartxt").innerHTML =
-            "Your guesses so far: " + YourguessesSoFar;
-        document.getElementById("lossestxt").innerHTML =
-            "losses: " + losses;
     }
 
-}
+
 
 // poplulates HTML on Page
+
+document.getElementById("winstxt").innerHTML = "Wins: " + wins;
+document.getElementById("lossestxt").innerHTML = "Losses: " + losses;
+document.getElementById("gussesLefttxt").innerHTML = "Guesses left: " + gussesLeft;
+document.getElementById("YourguessesSoFartxt").innerHTML = "Your guesses so far: " + yourguessesSoFar;
+}
+
+// document.write("wins: " + wins);
+// document.write("losses: " + losses);
+// document.write("Guesses Left: " + gussesLeft);
+// document.write("Your guesses so far: " + YourguessesSoFar);
+// document.write("computer picks:" + compGuess);
 
 
 
